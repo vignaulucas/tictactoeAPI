09af7dd0ae694dc0ae4d28df6970a606b73a8563
Lucas Vignau <123100387+vignaulucas@users.noreply.github.com>
2024-03-24 20:33:39 +0100

    Create webConfig.java

---
+++ src/main/java/com/postegresql/webConfig.java
@@ -0,0 +1,22 @@
+package com.postegresql;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${TIC_TAC_TOE_ORIGIN:http://localhost:3000}")
+    private String origin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(origin)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
